15dc8b484ac4c47d9d0d133ba751bfe94ab1eea2
beksus <85835904+beksus@users.noreply.github.com>
2025-08-06 14:00:53 +0800

    added comments inline code

---
+++ quiz/testing.py
@@ -0,0 +1,139 @@
+########## INSTRUCTIONS ##########
+# 1. Only add or modify codes within the blocks enclosed with
+#    ########## student's code ##########
+#
+#    ####################################
+##################################
+
+
+########## student's code ##########
+# If you need to import any library, you may do it here
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+
+####################################
+
+def student_details():
+  ########## student's code ##########
+  ##########    Task 1     ##########
+  # 1. Update the name and id to your name and student id
+  studentName = "Beksutlan Kirgizbaev"
+  studentId = "22078406"
+  ####################################
+  return studentName, studentId
+
+def load_file():
+  ########## student's code ##########
+  ##########    Task 2     ##########
+  # 1. load the csv file to be a pandas DataFrame with vairable name: "df"
+  #    (note that the csv file has no header)
+  #    add/set the headers of the columns (from left to right) to be
+  #        cache, channelmin, channelmax, publishedperformance, estimatedperformance
+
+  df = pd.read_csv("dataset.csv", header=None)
+  df.columns = ["cache", "channelmin", "channelmax", "publishedperformance", "estimatedperformance"]
+
+  ####################################
+  return df
+
+def train_clustering_model(df):
+  ########## student's code ##########
+  ##########    Task 3     ##########  
+  # 1. initialise a kmeans model with 5 clusters using variable name: "kmModel"
+  # 2. train the kmeans model using the following columns from df
+  #      publishedperformance, estimatedperformance
+
+  from sklearn.cluster import KMeans
+  kmModel = KMeans(n_clusters=5, random_state=42)
+  kmModel.fit(df[["publishedperformance", "estimatedperformance"]])
+
+  ####################################
+  return kmModel
+
+def test_clustering_model(df, kmModel):
+  ########## student's code ##########
+  ##########    Task 4     ##########  
+  # 1. use any 10 rows from df and identify/predict their clusters
+  # 2. save the identified cluster index with variable name: "outcome"
+
+  outcome = kmModel.predict(df[["publishedperformance", "estimatedperformance"]].head(10))
+
+  ####################################
+  return outcome
+
+def add_clustering_result_to_data(df, kmModel):
+  ########## student's code ##########
+  ##########    Task 5     ##########  
+  # 1. predict the clusters of every row in df
+  # 2. convert the cluster numbers (0,1,2,3,4,5) to alphabets (a,b,c,d,e)
+  # 3. add the cluster outcome as a new column called "cresult"
+
+  df['cresult'] = kmModel.predict(df[["publishedperformance", "estimatedperformance"]])
+  df['cresult'] = df['cresult'].apply(lambda x: chr(x + 97))  # Convert to 'a', 'b', 'c', 'd', 'e'
+
+  ####################################
+  return df
+
+def train_decision_tree(df):
+  ########## student's code ##########
+  ##########    Task 6     ##########  
+  # 1. initialise a decision tree model with maximum depth of 5 as variable: dtModel
+  # 2. train the decision tree model to classify based on inputs of
+  #    a. channelmin    
+  #    b. channelmax
+  #    to identify the output of "estimatedperformance"
+
+  from sklearn.tree import DecisionTreeClassifier
+  dtModel = DecisionTreeClassifier(max_depth=5, random_state=42)
+  dtModel.fit(df[['channelmin', 'channelmax']], df['estimatedperformance'])
+
+  ####################################
+  return dtModel
+
+def test_decision_tree(df, dtModel):
+  ########## student's code ##########
+  ##########    Task 7     ##########  
+  # 1. predict the class using the trained decision tree
+  # 2. add the predicted outcome as a new column called "dresult"
+
+  df['dresult'] = dtModel.predict(df[['channelmin', 'channelmax']])
+  df['dresult'] = df['dresult'].astype(str)
+
+  ####################################
+  return df
+
+def save_to_file(df):
+  ########## student's code ##########
+  ##########    Task 8     ##########  
+  # 1. save the dataframe "df" to a csv file with the name of "finalresults.csv"
+
+  df.to_csv("finalresults.csv", index=False)
+
+  ####################################
+
+if __name__ == "__main__": 
+  print("Only add or modify codes within the blocks enclosed with")
+  print("########## student's code ##########")
+  print("")
+  print("####################################")
+  print("DO NOT REMOVE OR MODIFY CODES FROM OTHER SECTIONS")
+  print("")
+
+  sname,sid = student_details()
+  print(f"You are {sname} with student ID {sid}")  
+
+  
+  ########## student's code ##########
+  # you do not need to change the code of this section
+  # but you may modify the code for debugging purpose
+  df = load_file()
+  kmModel = train_clustering_model(df)
+  results = test_clustering_model(df, kmModel)
+  df = add_clustering_result_to_data(df, kmModel)
+
+  dtModel = train_decision_tree(df)
+  df = test_decision_tree(df, dtModel)
+  save_to_file(df)
+  ####################################
